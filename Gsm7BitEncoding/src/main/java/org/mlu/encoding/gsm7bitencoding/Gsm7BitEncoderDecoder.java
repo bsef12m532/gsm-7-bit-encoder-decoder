b56e915e2c338c0cc5174d74fdda97bd2961890e
msincuba@gmail.com <msincuba@gmail.com@0a810fa1-6573-78ad-57fe-b8a4f35559ef>
2012-11-12 15:12:22 +0000

    Improved javadoc decumentation

--- Gsm7BitEncoding/src/main/java/org/mlu/encoding/gsm7bitencoding/Gsm7BitEncoderDecoder.java
+++ Gsm7BitEncoding/src/main/java/org/mlu/encoding/gsm7bitencoding/Gsm7BitEncoderDecoder.java
@@ -1,8 +1,31 @@
+/**
+ * Copyright 2012 Gsm7BitEncoding
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package org.mlu.encoding.gsm7bitencoding;
 
 /**
- * Hello world!
+ * 
+ * GSM 7 Bit implementation class
+ * 
+ * Generally you would extend this class 
  *
+ * File: Gsm7BitEncoderDecoder.java
+ * 
+ * @author Mlungisi Sincuba
+ * @since 1.0
  */
 public class Gsm7BitEncoderDecoder 
 {
